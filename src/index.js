5c688ec3353f078939cab7fc477faa4c21b5af1a
dlyfcl <fcldly@163.com>
2021-07-23 14:55:53 +0800

    关于代码分割提高首屏加载速度

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import fastclick from 'fastclick';
+
+fastclick.attach(document.body);
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Redirect } from "react-router-dom";
 import Home from '../application/Home/home';
-import Recommend from '../application/recommend/Recommend';
-import Singers from '../application/singers/Singers';
-import Singer from '../application/singers/Singer';
-import Rank from '../application/rank/Rank';
-import Album from '../application/Album/Album'
+
+// 我们可以对组件进行代码分割，达到组件懒加载的效果，这也是性能优化的一个手段，
+// 因为没必要在一开始加载所有组件，
+// 尤其在应用特别复杂、组件规模非常庞大的时候，这样可以大幅提升首屏加载速度。
+const RecommendComponent = lazy(() => import ('../application/recommend/Recommend'));
+const SingersComponent = lazy(() => import ('../application/singers/Singers'));
+const SingerComponent = lazy(() => import ('../application/singers/Singer'));
+const RankComponent = lazy(() => import ('../application/rank/Rank'));
+const AlbumComponent = lazy(() => import ('../application/Album/Album'));
+
+
+const SuspenseComponent = Component => props => {
+  return (
+    <Suspense fallback={null}>
+      <Component {...props}></Component>
+    </Suspense>
+  )
+}
 
 export default [
   {
@@ -21,31 +34,31 @@ export default [
       },
       {
         path: "/recommend",
-        component: Recommend,
+        component: SuspenseComponent(RecommendComponent),
         routes: [
           {
             path: "/recommend/:id",
-            component: Album
+            component: SuspenseComponent(AlbumComponent)
           }
         ]
       },
       {
         path: "/singers",
-        component: Singers,
+        component: SuspenseComponent(SingersComponent),
         routes: [
           {
             path: "/singers/:id",
-            component: Singer
+            component: SuspenseComponent(SingerComponent),
           }
         ]
       },
       {
         path: "/rank",
-        component: Rank,
+        component: SuspenseComponent(RankComponent),
         routes: [
           {
             path: "/rank/:id",
-            component: Album
+            component: SuspenseComponent(AlbumComponent),
           }
         ]
       }
